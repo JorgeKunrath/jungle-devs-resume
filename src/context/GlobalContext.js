446887c9409f2b7ad5c14108941ba69bb1ba3db1
Jorge Kunrath <jorgeks95@gmail.com>
2021-07-21 03:52:53 -0300

    add persistence

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,11 +1,20 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 export const GlobalContext = createContext(null)
 
 export function GlobalProvider({ children }) {
   const [finished, setFinished] = useState(false)
 
-  // TODO: get/set finished from LS
+  useEffect(() => {
+    if (finished) {
+      window.localStorage.setItem('finished', true)
+    } else {
+      try {
+        const lsFinished = window.localStorage.getItem('finished')
+        if (lsFinished) setFinished(true)
+      } catch (e) {}
+    }
+  }, [finished])
 
   return (
     <GlobalContext.Provider value={{ finished, setFinished }}>{children}</GlobalContext.Provider>
